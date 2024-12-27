a8935f7b91d040f01dbad3afcd71111922f1bd99
Sofibyte <27739941+Adorabus@users.noreply.github.com>
2024-12-27 12:33:27 -0800

    Remove unused hook and command

--- lua/autorun/server/sv_flybysounds.lua
+++ lua/autorun/server/sv_flybysounds.lua
@@ -1,16 +1,2 @@
 resource.AddSingleFile("sound/pink/flybysounds/fast_windloop1-louder.wav")
 resource.AddSingleFile("sound/pink/flybysounds/portal2_wind.wav")
-
--- NOTE: Unused
-hook.Add("Think","FlyBySound_ServerThink",function()
-    return
-end)
-
-concommand.Add("flybysounds_testsight",function(ply)
-    if SERVER and GetConVar("developer"):GetBool() then
-        local tr = ply:GetEyeTrace()
-        if IsValid(tr.Entity) then
-            print(tr.Entity:GetClass())
-        end
-    end
-end)
